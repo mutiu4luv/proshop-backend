0fb9118a877e96b2d924effb8799597b9ae1939d
mutiu4luv <88190404+mutiu4luv@users.noreply.github.com>
2022-03-11 17:25:06 +0100

    new

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import colors from "colors";
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -51,6 +53,9 @@ app.get("/api/config/paypal", (req, res) =>
 // app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.listen(
   PORT,
