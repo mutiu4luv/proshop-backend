63a75cbd5c67d6fa07d44ab6b1019c776e87e1fb
mutiu4luv <88190404+mutiu4luv@users.noreply.github.com>
2022-03-11 17:42:58 +0100

    new

--- server.js
+++ server.js
@@ -34,6 +34,31 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+app.use(express.json());
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "build/index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
 // const __dirname = path.resolve();
 // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
@@ -52,19 +77,19 @@ app.get("/api/config/paypal", (req, res) =>
 // app.use(notFound);
 // app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-);
+// app.listen(
+//   PORT,
+//   console.log(
+//     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+//   )
+// );
 
-app.use(express.static(path.join(__dirname, "/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "build/index.html"))
-);
+// app.use(express.static(path.join(__dirname, "/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "build/index.html"))
+// );
